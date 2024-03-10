ef428dabbb542a5bfd158bb01dd7dc6252b06f44
mayankchugh1977 <mayank.chugh@gmail.com>
2024-03-11 05:32:32 +0800

    addtional file and notes

---
+++ week_3_Functional_Programming_with_Python/module1.py
@@ -0,0 +1,14 @@
+
+courses=['C', 'C++', 'JAVA', 'PYTHON']
+
+def  artmeticoperation(num1, num2):
+    print("Addition is  :", num1+num2)
+    print("Subtraction is  :", num1-num2)
+    print("Multiplication  is  :", num1*num2)
+    print("Division  is  :", num1/num2)
+    
+def  compareoperation(num1, num2):
+    print(num1, "Greated than  :",num2,  num1>num2)
+    print(num1, "Less than  :",num2,  num1<num2)
+    print(num1, "Equal to   is  :",num2,  num1==num2)
+    print(num1, "Not equal to   is  :",num2,  num1!=num2)
---
+++ week_3_Functional_Programming_with_Python/printfile.py
@@ -0,0 +1,15 @@
+
+print("hello world")
+
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
